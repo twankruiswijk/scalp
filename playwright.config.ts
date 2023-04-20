5a85af421b04d73c0df89c6a5a28e6f539fb8ea9
twankruiswijk <twankrui@gmail.com>
2023-04-20 11:01:14 +0200

    prettier format current files

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: "./test",
+  testDir: './test',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: 'html',
   use: {
-    trace: "on-first-retry",
+    trace: 'on-first-retry',
   },
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
   ],
 });
--- test/padel.spec.ts
+++ test/padel.spec.ts
@@ -1,20 +1,20 @@
-import { test } from "@playwright/test";
+import { test } from '@playwright/test';
 
-const bookUrl = "https://meetandplay.nl/club/62745";
+const bookUrl = 'https://meetandplay.nl/club/62745';
 
-test("test", async ({ page }) => {
+test('test', async ({ page }) => {
   await page.goto(bookUrl);
   await page
-    .getByRole("button", { name: "Alle cookies accepteren" })
+    .getByRole('button', { name: 'Alle cookies accepteren' })
     .click({ timeout: 5000 });
 
-  await page.getByText("15:30").click();
+  await page.getByText('15:30').click();
   await page
-    .locator("a")
+    .locator('a')
     .filter({
       hasText:
-        "20 apr. 15:30 - 16:30 60 minuten JPB Padelbaan 60 minuten · 15:30 - 16:30 € 20,0",
+        '20 apr. 15:30 - 16:30 60 minuten JPB Padelbaan 60 minuten · 15:30 - 16:30 € 20,0',
     })
-    .getByRole("button", { name: "Boeken" })
+    .getByRole('button', { name: 'Boeken' })
     .click();
 });
