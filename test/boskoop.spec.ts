7a9230d25a4a0d51e8cab1da77e42e32293a4c5f
Mike Woudenberg <mwoudenberg@xebia.com>
2023-04-20 15:59:46 +0200

    Adds mail notification

--- test/boskoop.spec.ts
+++ test/boskoop.spec.ts
@@ -1,4 +1,8 @@
+import { add, format } from 'date-fns';
 import { test, expect, Page } from '../fixtures/test';
+import sendGrid from '@sendgrid/mail';
+
+sendGrid.setApiKey(process.env.SENDGRID_APIKEY!);
 
 async function authenticateAccount(page: Page) {
   await page.goto('https://middenboskoop.baanreserveren.nl/');
@@ -17,7 +21,7 @@ const desiredStartTimes = ['19:30', '20:00', '20:30'];
 
 test(authenticateAccount)('test', async ({ page }) => {
   await page.goto(
-    'https://middenboskoop.baanreserveren.nl/reservations/2023-04-27/sport/1272'
+    'https://middenboskoop.baanreserveren.nl/reservations/2023-04-28/sport/1272'
   );
 
   await page
@@ -37,6 +41,15 @@ test(authenticateAccount)('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Betalen' }).click();
 
   await expect(page).toHaveURL(/https:\/\/www.mollie.com\/checkout\//);
-  // TODO notify Bart
-  console.log('Now at', page.url());
+
+  const linkExpiryDate = format(
+    add(new Date(), { minutes: 10 }),
+    'dd-MM-yyyy HH:mm'
+  );
+  await sendGrid.send({
+    from: 'twan.kruiswijk@xebia.com',
+    to: 'bdenhollander@xebia.com',
+    subject: 'Padelbaan gereserveerd!',
+    html: `<div>Er is een baan voor je gereserveerd. Betaal via <a href="${page.url()}">deze link</a>. Je hebt tot ${linkExpiryDate} om te betalen</div>`,
+  });
 });
